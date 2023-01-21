057f127f30c4814e8b5717a29533a0d7bb950c33
gemansipator <gemansipator@gmail.com>
2023-01-21 13:39:09 +0400

    Напишите метод, на вход которого подается двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо бросить исключение MyArraySizeException. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException – с детализацией, в какой именно ячейке лежат неверные данные. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.

---
+++ src/lesson03/home/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package lesson03.home;
+
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException{   //исключение "неправильный размер массива"
+
+    //    MyArraySizeException(String msg) {
+//        super(msg);
+//    }
+}
